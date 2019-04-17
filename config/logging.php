5b20254899451ced1a3cdfb0475cca27217ade30
Dan Harrin <git@DanHarrin.com>
2019-04-17 12:18:00 +0100

    0.10.6

--- config/logging.php
+++ config/logging.php
@@ -39,7 +39,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => env('LOG_DRIVER', 'single'),
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],

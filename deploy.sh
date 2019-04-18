38a81f25cd59fc38e223414a45a9b6f73e91770c
Dan Harrin <git@DanHarrin.com>
2019-04-18 07:33:58 +0100

    0.15

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 php artisan down
 php artisan migrate --force
-php artisan cache:clear
 php artisan auth:clear-resets
-php artisan route:clear
-php artisan route:cache
-php artisan config:clear
-php artisan config:cache
 php artisan up
